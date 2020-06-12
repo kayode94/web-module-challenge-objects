5334060877fa2c259e25618e2d5693dd42b205fc
kayode <kayode.richards@gmail.com>
2020-06-12 09:15:32 -0400

    added JS to tasks 1-8

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ const breakfastBurrito = {name: "Breakfast Burrito", price: 16, category:"Breakf
 /* Task 1a: write a function to return more menu items with the same format as the items above. */
 
 function createMenuItem(name, cost, category){
-    /* Code here */
+    return {name, cost, category}
 }
 
 /* Task 1b: use your function to create 3 more menu items. You may add any items to the menu that you'd like */
 
-
+console.log(createMenuItem());
+console.log(createMenuItem());
+console.log(createMenuItem());
 
 /* Task 2: You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to your burger object that automatically calculates price given a string as a parameter. 
 
@@ -24,7 +26,19 @@ and should return a number.
 
 For example, burger.discount("teacher") would return 13.5 and burger.discount("public") would return 16.2*/
 
+burger.discount = function (string){
+  if(string === 'teacher'){
+    console.log(burger.price - burger.price * .25);
+  } else if (string === 'student'){
+    console.log(burger.price - burger.price * .25);
+  } else if (string === 'public'){
+    console.log(burger.price - burger.price * .10);
+  } else {
+    console.log('who are you?')
+  }
+}
 
+burger.discount('teacher');
 
 ///////////////Reviews (MVP)///////////////////
 
@@ -39,12 +53,24 @@ const reviews = [{name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and
 ]
 
 /* Task 4: Console.log just Julius' feedback */
-
+console.log(reviews[5].feedback)
 
 /* Task 5: Add a new rating with your (fictitious) opinions of the restaurant in the same format as the reviews above. */
+function makeReview(name, rating, feedback){
+  return {name, rating, feedback};
+}
 
+console.log(makeReview('Kay', 4, 'Cool vibe, great food'));
 
-/* Task 6: Add the following feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
+/* Task 6: Add the following feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"*/
+
+function addFeedback(array/*the array we want to edit*/, index/*the index of the array*/, feedback/*the value we want to change*/){
+  array[index].feedback = feedback;
+  return array;
+}
+
+console.log(addFeedback(reviews/*the first parameter*/, 7/*the second parameter*/, "this place is chill with really cool people, great for getting work done on weekdays"/*the third parameter*/));
+
 
 /*  Task 7: Write a function to return a review based on the index of the review in the array.
 
@@ -58,9 +84,10 @@ and should return a string in the format `{name} gave the restaurant a {rating},
  * For example, if getReviewByIndex is invoked with reviews and the number 0
  * it will return `Daniela gave the restaurant a 5 star review and their feedback was: Beautiful atmosphere and wonderful vegan options!`
 */
-function getReviewByIndex(reviews, index) {
-    /* code here */
-  }
+function getReviewByIndex(reviews, index){
+  return `${reviews.name} gave the restaurant a ${reviews.rating} and their feedback was: ${reviews.feedback}`
+}
+  console.log(getReviewByIndex(reviews[0]))
   
 
 /* Task 8: Write a function to get information about the most recent review called `getLastReview`
@@ -72,10 +99,16 @@ and should return a string in the format `name} gave the restaurant a {rating}, 
 
 For example, if getLastReview is invoked passing the reviews array it will return `Reyna gave the restaurant a 3.5 star review and their feedback was: "this place is chill with really cool people, great for getting work done on weekdays"`.
 */
-function getLastReview(/* code here */) {
-    /* code here */
-  } 
-
+function getLastReview(arr) {
+  const index = (arr.length - 1)
+  name = arr[index].name
+  rating = arr[index].rating;
+  feedback = arr[index].feedback;
+  if(arr[index]){
+    return `${name} gave the restaurant a ${rating}, and their feedback was: ${feedback}`;
+  }
+} 
+console.log(getLastReview(reviews));
 
 ///////////////🍔☕️🍽 STRETCH🍔☕️🍽////////////////////
 
